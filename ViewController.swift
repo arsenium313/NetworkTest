3417e8d509962684c5eb7bf8a298394e567df18b
arsenium313 <arsenium313@gmail.com>
2023-04-17 15:08:19 +0300

    Init commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkTest
+//
+//  Created by Арсений Кухарев on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
